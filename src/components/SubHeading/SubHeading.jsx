b959645e889814c4d90e27d3a22b040cf4f08716
ahmed <saiefeddina@gmail.com>
2022-03-21 16:51:23 +0100

    up

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+import spoon from '../../assets/spoon.png';
+
+const SubHeading = ({ title }) => (
+  <div style={{ marginBottom: '1rem' }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={spoon} alt="spoon_image" className="spoon__img" />
   </div>
 );
 
